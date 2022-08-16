209625de51e213d34662a4ae693766e84cb103a0
JoanDGG <ggjoan2000@gmail.com>
2022-08-16 13:56:41 -0500

    Basic movement & jump input

---
+++ Assets/PlayerMovement3D.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public class PlayerMovement3D : MonoBehaviour
+{
+    [Header("Horizontal movement")]
+    [Tooltip("Variables needed for character movement and camera functionality")]
+    public float speed = 6f;
+    private Vector3 direction;
+    new private Rigidbody rigidbody;
+
+    [Header("Camera movement")]
+    [Tooltip("Variables needed for camera turning")]
+    new public Transform camera;
+    public float turnTime = 0.1f;
+    public float turnVelocity;
+
+    [Header("Vertical movement")]
+    [Tooltip("Variables needed for gravity functionality")]
+    public float jumpHeight = 8f;
+    public float fallMultiplier = 2.5f;     // Value determining how fast the player will fall
+    public float lowJumpMultiplier = 2f;    // Value determining how far will the long jump go
+    private float gravity =  -9.81f;
+    private Vector3 velocity;
+
+
+    [Header("Ground controller")]
+    [Tooltip("Variables needed to check ground below player")]
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckGrounded();
+        CheckInputs();
+        CheckMove();
+        CheckJump();
+    }
+
+    private void CheckGrounded()
+    {
+        //Check if object is grounded by creating an invisible sphere
+        //and checking if anything contained in groundMask is in contact with it
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+    }
+
+    private void CheckInputs()
+    {
+        //Gather Keyboard Input and create resulting vector
+        //Normalized to avoid faster movement in diagonals
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+    }
+
+    private void CheckJump()
+    {
+        //Gravity Control
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rigidbody.velocity = new Vector3(velocity.x, 0f, velocity.z);
+            rigidbody.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        }
+
+        // Manage Long/short jump
+        if(rigidbody.velocity.y < 0)
+        {
+            rigidbody.velocity += Vector3.up * gravity * (fallMultiplier - 1) * Time.deltaTime;
+        }else if(rigidbody.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rigidbody.velocity += Vector3.up * gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    private void CheckMove()
+    {
+        if(direction.magnitude >= 0.1f)
+        {
+            //Utilize Atan2 function to find angle player should look at based on direction vector and camera angle
+            //Utilize SmoothDampAngle function to change the angle based on established variables for a smoother look
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
+            float resultAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnVelocity, turnTime);
+            transform.rotation = Quaternion.Euler(0f, resultAngle, 0f);
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            rigidbody.AddForce(moveDirection.normalized * speed);
+        }
+    }
+}
