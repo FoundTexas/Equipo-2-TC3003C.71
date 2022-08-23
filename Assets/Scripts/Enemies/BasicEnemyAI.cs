5fa5455f8324e83d8e1a6f2bf3f19f3e171371d5
Seidmani64 <A01028650@itesm.mx>
2022-08-22 19:10:09 -0500

    Added first instance of basic enemy

---
+++ Assets/Scripts/Enemies/BasicEnemyAI.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicEnemyAI : MonoBehaviour
+{
+    //Variables needed for enemy calculations
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask isGround, isPlayer;
+
+
+    //Patrolling variables
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking variables
+    public float attackSpeed;
+    bool hasAttacked;
+
+    //State variables
+    public float sightRange, attackRange;
+    public bool playerInSights, playerInRange;
+
+    Animator animator;
+
+    void Awake()
+    {
+        player = GameObject.Find("Robot").transform;
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check sight and attack range
+        playerInSights = Physics.CheckSphere(transform.position, sightRange, isPlayer);
+        playerInRange = Physics.CheckSphere(transform.position, attackRange, isPlayer);
+    
+        //Set appropriate state based on current position of player
+        //in comparison to enemy
+        if(!playerInSights && !playerInRange)
+            Patrolling();
+        if(playerInSights && !playerInRange)
+            Chasing();
+        if(playerInSights && playerInRange)
+            Attacking();
+    
+    
+    }
+
+    void Patrolling()
+    {
+        if(!walkPointSet)
+            CreateWalkPoint();
+
+        if(walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToGoal = transform.position - walkPoint;
+
+        //Reached the destination, repeat to find a new one
+        if(distanceToGoal.magnitude < 1f)
+            walkPointSet = false;
+    }
+
+    void Chasing()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    void Attacking()
+    {
+        //Stop enemy movement and look at player
+        agent.SetDestination(transform.position);
+        animator.SetTrigger("Attack");
+        transform.LookAt(player);
+
+
+        if(!hasAttacked)
+        {
+            hasAttacked = true;
+            Invoke(nameof(ResetAttack), attackSpeed);
+        }
+
+    }
+
+    void CreateWalkPoint()
+    {
+        //Generate a random point in enemy range
+        float randX = Random.Range(-walkPointRange, walkPointRange);
+        float randZ = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randX, transform.position.y, transform.position.z + randZ);
+
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, isGround))
+            walkPointSet = true;
+    }
+
+    void ResetAttack()
+    {
+        hasAttacked = false;
+    }
+
+}
