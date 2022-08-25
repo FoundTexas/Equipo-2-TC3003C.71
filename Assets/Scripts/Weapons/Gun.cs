f6696b2446f335d7288e8d9e8144477976cb9951
CharlesBoop <nc.charly.161@hotmail.com>
2022-08-25 17:37:38 -0500

    Lookin for some gun stuff baby you see it.
    Basic gun stuff

---
+++ Assets/Scripts/Weapons/Gun.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : Weapon
+{
+
+    [SerializeField]
+    public Vector3 BulletSpreadVariance = new Vector3(0.1f,0.1f,0.1f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
