32b7a18a5ac50fd29f713d511c946897e2124892
Seidmani64 <A01028650@itesm.mx>
2022-09-01 20:01:08 -0500

    Initial health bar commit

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour, IDamage
+{
+
+    float hp;
+    float maxHp = 6;
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = maxHp;
+        healthBar.SetMaxHealth(maxHp);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            TakeDamage(1);
+        }
+    }   
+    
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            TakeDamage(1);
+        }
+    } 
+
+    public void AddHealth(float amount)
+    {
+        hp += amount;
+        healthBar.SetHealth(hp);
+    }
+
+    public void Die()
+    {
+        Destroy(this.gameObject);
+    }
+
+    public virtual void TakeDamage(float dmg)
+    {
+        hp -= dmg;
+        healthBar.SetHealth(hp);
+
+        if (hp <= -1)
+        {
+            Die();
+        }
+    }
+
+    public void Freeze()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Burn()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IEnumerator Burnning()
+    {
+        throw new System.NotImplementedException();
+    }
+}
