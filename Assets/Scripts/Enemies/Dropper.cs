d1efb42073993921021c5dbaea412a90f57b204f
JoanDGG <ggjoan2000@gmail.com>
2022-09-04 23:18:44 -0500

    Added Collectibles

---
+++ Assets/Scripts/Enemies/Dropper.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    
+    [Header("Stats")]
+    [Range(0, 1)] public float dropEnergy = 0.4f; 
+    [Range(0, 1)] public float dropCurrency = 0.6f; 
+    
+    [Header("Prefabs")]
+    public GameObject EnergyPrefab;
+    public GameObject CurrencyPrefab;
+    
+    void OnDestroy()
+    {
+        if(Random.Range(0f, 1f) >= dropEnergy)
+        {
+            Vector3 randomPosition = transform.position + Random.insideUnitSphere;
+            Instantiate(EnergyPrefab, randomPosition, Quaternion.identity);
+        }
+        if(Random.Range(0f, 1f) >= dropCurrency)
+        {
+            //Player obtained currency
+            Vector3 randomPosition = transform.position + Random.insideUnitSphere;
+            Instantiate(CurrencyPrefab, randomPosition, Quaternion.identity);
+        }
+    }
+}
