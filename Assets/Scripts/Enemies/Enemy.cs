d7ade3e4690ba0b4fc110a0895988f708da5cdfe
Seidmani64 <A01028650@itesm.mx>
2022-09-12 19:29:19 -0500

    Fixed enemies getting stuck on walls

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -32,6 +32,7 @@ public class Enemy : MonoBehaviour, IDamage
 
     public HitStop hitStop;
     public GameObject explosionfx;
+    public float timePatrolling = 0f;
 
     void Awake()
     {
@@ -81,9 +82,15 @@ public class Enemy : MonoBehaviour, IDamage
 
         Vector3 distanceToGoal = transform.position - walkPoint;
 
+        timePatrolling += Time.deltaTime;
+
         //Reached the destination, repeat to find a new one
-        if(distanceToGoal.magnitude < 1f)
+        if(distanceToGoal.magnitude < 1f || timePatrolling >= 3f)
+        {
             walkPointSet = false;
+            timePatrolling = 0f;
+        }
+        
     }
 
     public void Chasing()
