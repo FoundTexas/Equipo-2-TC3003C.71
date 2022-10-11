6663f3e6ab563eeb8a40d500c6c7fd57f9ecb38f
FoundTexas <rolega01@gmail.com>
2022-10-10 22:09:15 -0500

    ToonShader Revamp

---
+++ Assets/Fonts/InputsFont/InputTypeCheck.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class InputTypeCheck : MonoBehaviour
+{
+    public string device;
+    private void Update() {
+        device = InputDevice.deviceId;
+
+
+    }
+
+}
